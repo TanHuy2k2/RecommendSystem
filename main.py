2b5cf1d20669cf0e92be1e15309d774df6286b52
TanHuy652k2 <tanhuyqn6789@gmail.com>
2025-03-21 17:13:59 +0700

    new

--- main.py
+++ main.py
@@ -20,20 +20,26 @@ def index():
 @app.route('/search', methods=['GET'])
 def search_food():
     query = request.args.get('query', '').lower()
-    recommended = predict_recipe(query)
+    if query == '':
+        return jsonify(foods[:10])
+    else:
+        recommended = predict_recipe(query)
 
-    quantities, ingredients = extract_ingredient(query)
+        quantities, ingredients = extract_ingredient(query)
 
-    X = []
-    for i, j in zip(ingredients, quantities):
-       X.append({"name": i, "quantity": str(j)})
+        X = []
+        for i, j in zip(ingredients, quantities):
+            X.append({"name": i, "quantity": str(j)})
 
-    recommendations = recommend_dishes(X, df_main, limit=10)
- 
-    common_ids = list(set(recommendations).intersection(recommended['recipe_id'].values))
-    results = [food for food in foods if food['recipe_id'] in common_ids]
+        filtered_recipes = df_main.loc[df_main["recipe_id"].isin(recommended['recipe_id'].values), ["recipe_id", "ingredients_processed"]]
 
-    return jsonify(results)
+        recommendations = recommend_dishes(X, filtered_recipes)
+    
+        results = [food for food in foods if food['recipe_id'] in recommendations]
+
+        if results:
+            return jsonify(results)
+        return jsonify([])
 
 # Route to send the first 10 foods by default
 @app.route('/foods', methods=['GET'])
--- search_for_ingredients.py
+++ search_for_ingredients.py
@@ -1,35 +1,28 @@
 from convert_quantity import convert_quantity
 
-def recommend_dishes(user_ingredients, df, limit, threshold=0.5):
-     recommended_dishes = []
-     
-     # Duyệt qua từng món ăn
-     for _, row in df.iterrows():
-         dish_id = row['recipe_id']
-         ingredients = row['ingredients_processed']
-         
-         matched_count = 0  # Đếm số nguyên liệu phù hợp
-         total_ingredients = len(user_ingredients)  # Tổng số nguyên liệu đầu vào
-         
-         for user_ingredient in user_ingredients:
+def recommend_dishes(user_ingredients, df, threshold=0.5):
+    recommended_dishes = []
+    for _, row in df.iterrows():
+        dish_id = row['recipe_id']
+        ingredients = row['ingredients_processed']
+        
+        matched_count = 0
+
+        total_ingredients = len(user_ingredients)
+
+        for user_ingredient in user_ingredients:
              user_name = user_ingredient['name'].lower()
              user_qty = convert_quantity(user_ingredient['quantity'])
-             
+
              for ingredient in ingredients:
                 ingredient_name = ingredient['name'].lower()
                 ingredient_qty = convert_quantity(ingredient['quantity'])
-                 
-                if user_name == ingredient_name and ingredient_qty <= user_qty:
-                     matched_count += 1
-                     break  # Chỉ cần tìm thấy 1 lần thì dừng vòng lặp
- 
-         # Kiểm tra tỷ lệ nguyên liệu phù hợp
-         match_ratio = matched_count / total_ingredients
-         
-         if match_ratio >= threshold:
-             recommended_dishes.append(dish_id)
-         
-         if len(recommended_dishes) >= limit:
-             break
-     
-     return recommended_dishes
+
+                if user_name in ingredient_name and ingredient_qty <= user_qty:
+                    matched_count += 1
+
+                    match_ratio = matched_count / total_ingredients
+
+                    if match_ratio >= threshold:
+                          recommended_dishes.append(dish_id)
+    return recommended_dishes
