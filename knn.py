f6c4408c87cc7c94e468e7ae484bd99988b97ed6
TanHuy652k2 <tanhuyqn6789@gmail.com>
2025-03-24 11:38:05 +0700

    new

--- knn.py
+++ knn.py
@@ -18,7 +18,7 @@ vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(df['ingredients'])
 
 # Tạo mô hình Nearest Neighbors
-knn = NearestNeighbors(n_neighbors=10, metric='cosine')
+knn = NearestNeighbors(n_neighbors=12, metric='cosine')
 knn.fit(X)
 
 # Hàm dự đoán món ăn dựa trên nguyên liệu nhập vào
